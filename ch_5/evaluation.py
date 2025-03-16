b6520b33f082f4a68003223ac2ce1767630d67bb
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2025-03-16 22:40:09 +0300

    Create evaluation.py

---
+++ ch_5/evaluation.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def evaluate_model(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_true, y_pred)
+    return mse, mae, rmse, r2
