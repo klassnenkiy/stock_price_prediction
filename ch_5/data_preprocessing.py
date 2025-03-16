8bdc3722b0d88594b26dca38722b34957726df1d
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2025-03-16 22:37:55 +0300

    Create data_preprocessing.py

---
+++ ch_5/data_preprocessing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def filter_data_by_tickers(data, tickers):
+    return data[data['TICKER'].isin(tickers)]
+
+def preprocess_data(data):
+    data['TRADEDATE'] = pd.to_datetime(data['TRADEDATE'])
+    # Можно добавить другие шаги предобработки
+    return data
