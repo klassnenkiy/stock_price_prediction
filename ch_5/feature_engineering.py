db581143995e378963880016ba012636439b7690
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2025-03-16 22:38:50 +0300

    Create feature_engineering.py

---
+++ ch_5/feature_engineering.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def create_lag_features(data, window_size):
+    X, y = [], []
+    for i in range(window_size, len(data)):
+        X.append(data[i-window_size:i])
+        y.append(data[i])
+    return np.array(X), np.array(y)
